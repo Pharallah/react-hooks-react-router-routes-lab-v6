56a6d412243a57a4dd918346fff1a72fd1bf4ccd
Harry San Juan <harrycsanjuan@gmail.com>
2024-02-25 19:32:06 -0500

    completed lab

--- src/pages/Actors.js
+++ src/pages/Actors.js
@@ -1,13 +1,38 @@
 import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar"
+
 
 function Actors() {
+  const [actors, setActors] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:4000/actors")
+      .then(res => res.json())
+      .then(data => setActors(data))
+  }, [])
+
+  // DISPLAY ACTORS
+  const displayActors = actors.map(actor => {
+    
+    //ITERATE THRU ACTOR MOVIES
+    const displayMovies = actor.movies.map(movie => {
+      return <li key={movie}>{movie}</li>
+    })
+
+    return <article key={actor.id}>
+      <h2>{actor.name}</h2>
+      <ul>{displayMovies}</ul>
+    </article>
+  })
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Actor info here! */}
+        <h1>Actors Page</h1>
+        {displayActors}
       </main>
     </>
   );
--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -1,13 +1,38 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import NavBar from "../components/NavBar";
 
 function Movie() {
+  const [currentMovie, setCurrentMovie] = useState({})
+  const [genres, setGenres] = useState([])
+  const params = useParams();
+  const movieId = params.id
+
+  // UTILIZE URL PARAMS
+  useEffect(() => {
+    fetch(`http://localhost:4000/movies/${movieId}`)
+      .then(res => res.json())
+      .then(data => {
+        setCurrentMovie(data)
+        setGenres(data.genres)
+      })
+      .catch(error => console.log(error))
+  }, [movieId])
+
+  // ITERATE THRU GENRES STATE
+  const displayGenres = genres.map(genre => {
+    return <span key={genre}>{genre}</span>
+  })
+  
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Movie info here! */}
+        <h1>{currentMovie.title}</h1>
+        <p>Runtime: {currentMovie.time} minutes</p>
+        {displayGenres}
       </main>
     </>
   );
